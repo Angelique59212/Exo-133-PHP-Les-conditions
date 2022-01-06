c93039d23ec4efd882c3ccbaf0e13d0e194b8700
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-06 18:45:58 +0100

    condition

--- index.php
+++ index.php
@@ -4,44 +4,53 @@
 $a = 50;
 $b = 10;
 
-       >    {
+if ($a > $b) {
     echo "Hello World!";
-}
+};
+
 
 //Deuxieme ligne
-    {
-    echo "<br><br>Hello World2";
-}
+if (!($a <= $b)) {
+     echo "<br><br>Hello World2";
+};
+
 
 //Troisieme ligne
-($a==b) {
+if ($a==$b) {
     echo "<br><br>Oui";
 }
-
+else
 {
     echo "<br><br>Non";
-}
-
+};
 
 //Quatrieme ligne
 echo"<br><br>";
- ($a == $b) {
+if($a == $b)
+{
     echo "1";
-}  ($a > $b) {
+}
+elseif ($a > $b)
+{
     echo "2";
 }
+else
 {
     echo "3";
 }
 
-
 //Cinquieme ligne
 echo"<br><br>";
 
- ($color) {
-    "red":
-    echo "Hello";
-   "green":
-    echo "Welcome";
+$color = "red";
+switch ($color)
+{
+    case "red":
+        echo "hello";
+        break;
+    case "green":
+        echo "welcome";
+        break;
+    default:
+        echo "Dzień dobry";
 }
-
